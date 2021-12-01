ca3e294a4738bfc9d22f0b1af31467cc9c0dc05b
cheropChirchir7963 <cheropmarlene@gmail.com>
2021-12-01 16:25:44 +0300

    added landing and launching tests

--- test/fuel_calc_test.exs
+++ test/fuel_calc_test.exs
@@ -2,7 +2,10 @@ defmodule FuelCalcTest do
   use ExUnit.Case
   doctest FuelCalc
 
-  test "greets the world" do
-    assert FuelCalc.hello() == :world
+  test "landing_fuel/2" do
+    assert FuelCalc.land_fuel(28801, 9.087) == 9278
+  end
+  test "launching_fuel/2" do
+    assert FuelCalc.launch_fuel(28801, 9.087) == 10959
   end
 end
