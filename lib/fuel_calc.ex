ded6ed5bf21e2b19f9317042520a0a192d91f1a0
cheropChirchir7963 <cheropmarlene@gmail.com>
2021-12-02 06:45:59 +0300

    added fuel calculations

--- lib/fuel_calc.ex
+++ lib/fuel_calc.ex
@@ -3,16 +3,15 @@ defmodule FuelCalc do
   Documentation for FuelCalc.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> FuelCalc.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def land_fuel(mass, gravity) do
+    fuel = mass * gravity * 0.033 - 42
+    trunc(fuel)
   end
+
+  def launch_fuel(mass, gravity) do
+    fuel = mass * gravity * 0.042 - 33
+    trunc(fuel)
+  end
+
 end
+0
--- test/fuel_calc_test.exs
+++ test/fuel_calc_test.exs
@@ -3,9 +3,10 @@ defmodule FuelCalcTest do
   doctest FuelCalc
 
   test "landing_fuel/2" do
-    assert FuelCalc.land_fuel(28801, 9.087) == 9278
+    assert FuelCalc.land_fuel(28801, 9.807) == 9278
   end
+
   test "launching_fuel/2" do
-    assert FuelCalc.launch_fuel(28801, 9.087) == 10959
+    assert FuelCalc.launch_fuel(28801, 9.807) == 11829
   end
 end
